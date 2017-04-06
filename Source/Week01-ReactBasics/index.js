ca31e5f5eab9ccff64e5db4fa3524599f9f78c3b
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-04-06 15:57:54 -0700

    I made a react component

---
+++ Source/Week01-ReactBasics/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by fish on 4/6/17.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ReactBasics} from './ReactBasics.js';
+
+ReactDOM.render(<ReactBasics/>, document.getElementById('root'));
