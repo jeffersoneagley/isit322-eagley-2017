0881daae707dbda347e3d95733a9be38c80c57f5
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-04-06 15:24:25 -0700

    derp!

---
+++ Source/test/src/Bar.js
@@ -0,0 +1,15 @@
+/**
+ * Created by fish on 4/4/17.
+ */
+import React from 'react';
+
+
+class Bar extends React.Component {
+    render() {
+        return (
+            <h2>Bar</h2>
+        )
+    }
+}
+
+export default Bar;
--- Source/test/src/index.js
+++ Source/test/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Bar from './Bar';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <div>
+        <App />
+        <Bar />
+    </div>,
+    document.getElementById('root')
 );
