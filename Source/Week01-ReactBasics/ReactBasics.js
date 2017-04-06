bb34a7b09e4d335e60a249e252cb7623001f0499
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-04-06 16:39:24 -0700

    a few changes

--- Source/Week01-ReactBasics/ReactBasics.js
+++ Source/Week01-ReactBasics/ReactBasics.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export class ReactBasics extends React.Component {
     render() {
-        return <h1>An H1 element in a React Component</h1>
+        return (
+            <div id="parent_div_for_render_return">
+                <h1>An H1 element in a React Component</h1>
+                < p > Some
+                    text
+                </ p >
+            </div>
+        );
     }
 }
