4f82a4ffe688d43a09bae0b606a6a145ce0ff5c9
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-04-06 17:17:26 -0700

    second module added

---
+++ Source/Week01-ReactBasics/ReactHello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class ReactHello extends React.Component {
+    render() {
+        return (
+            <div id="parent_div_for_render_return">
+                <h1>React Hello</h1>
+                < p >
+                    this is the second module!
+                </ p >
+            </div>
+        );
+    }
+}
--- Source/Week01-ReactBasics/main.js
+++ Source/Week01-ReactBasics/main.js
@@ -4,5 +4,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {ReactBasics} from './ReactBasics.js';
+import {ReactHello} from './ReactHello.js';
 
-ReactDOM.render(<ReactBasics/>, document.getElementById('root'));
+ReactDOM.render(<div>
+    <ReactBasics/>
+    <ReactHello/>
+</div>, document.getElementById('root'));
