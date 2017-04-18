72412d6bdc848cb23e3fcfdbda23db16418b1502
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-04-17 23:54:46 -0700

    built out an autofiller for the paragraphs

--- Source/week02-rest-basics/client/src/App.js
+++ Source/week02-rest-basics/client/src/App.js
@@ -4,15 +4,67 @@ import './App.css';
 import 'whatwg-fetch';
 var fetch = require('./mocks/fetch').fetch;
 
+var nameDummies = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine'
+];
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            file: 'Get Nine Result will be placed here',
-            foo : 'waiting for server',
-            nine: '0'
+            file : 'Get Nine Result will be placed here',
+            foo  : 'waiting for server',
+            one  : '0',
+            two  : '0',
+            three: '0',
+            four : '0',
+            five : '0',
+            six  : '0',
+            seven: '0',
+            eight: '0',
+            nine : '0'
         }
     };
+
+    getOne = () => {
+        this.setState({one: '1'});
+    }
+
+    getTwo = () => {
+        this.setState({two: '2'});
+    }
+
+    getThree = () => {
+        this.setState({three: '3'});
+    }
+
+    getFour = () => {
+        this.setState({four: '4'});
+    }
+
+    getFive = () => {
+        this.setState({five: '5'});
+    }
+
+    getSix = () => {
+        this.setState({six: '6'});
+    }
+
+    getSeven = () => {
+        this.setState({seven: '7'});
+    }
+
+    getEight = () => {
+        this.setState({eight: '8'});
+    }
 
     getNine = () => {
         this.setState({nine: '9'});
@@ -31,6 +83,20 @@ class App extends Component {
         });
     };
 
+    getParagraph = (index) => {
+        return (<p id={"para" + index} key={"para" + index}>
+            {index}: {this.state[index]}
+        </p>);
+    };
+
+    getParagraphs = () => {
+        var res = [];
+        for (var val in nameDummies) {
+            res.push(this.getParagraph(nameDummies[val]));
+        }
+        return res;
+    };
+
     render() {
         return (
             <div className="App">
@@ -44,10 +110,19 @@ class App extends Component {
                 <p className="App-intro">
                     file: {this.state.file}
                 </p>
-                <p className="App-intro">
-                    nine: {this.state.nine}
-                </p>
+                {this.getParagraphs()}
+                {/*<p className="App-intro">*/}
+                {/*nine: {this.state.nine}*/}
+                {/*</p>*/}
                 <button id="buttonFoo" onClick={this.bar}>Bar</button>
+                <button id="buttonOne" onClick={this.getOne}>GetOne</button>
+                <button id="buttonTwo" onClick={this.getTwo}>GetTwo</button>
+                <button id="buttonThree" onClick={this.getThree}>GetThree</button>
+                <button id="buttonFour" onClick={this.getFour}>GetFour</button>
+                <button id="buttonFive" onClick={this.getFive}>GetFive</button>
+                <button id="buttonSix" onClick={this.getSix}>GetSix</button>
+                <button id="buttonSeven" onClick={this.getSeven}>GetSeven</button>
+                <button id="buttonEight" onClick={this.getEight}>GetEight</button>
                 <button id="buttonNine" onClick={this.getNine}>GetNine</button>
             </div>
         );
--- Source/week02-rest-basics/client/src/App.test.js
+++ Source/week02-rest-basics/client/src/App.test.js
@@ -4,6 +4,22 @@ import App from './App';
 import {shallow} from 'enzyme';
 
 describe('my basic rest test', function () {
+    var simNames = [
+        {name: 'one', short: '1'},
+        {name: 'two', short: '2'},
+        {name: 'three', short: '3'},
+        {name: 'four', short: '4'},
+        {name: 'five', short: '5'},
+        {name: 'six', short: '6'},
+        {name: 'seven', short: '7'},
+        {name: 'eight', short: '8'},
+        {name: 'nine', short: '9'}
+
+    ];
+    simNames.forEach((val) => {
+        val.upperName = val.name.charAt(0).toUpperCase() + val.name.slice(1);
+    })
+
     it('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<App />, div);
@@ -15,18 +31,89 @@ describe('my basic rest test', function () {
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
+
     it('renders getnine target', () => {
         const wrapper = shallow(<App />);
-        const nineSign = <p className="App-intro">nine: 0</p>;
-        expect(wrapper.contains(nineSign)).toEqual(true);
+        const numberElement = <p className="App-intro">nine: 0</p>;
+        expect(wrapper.contains(numberElement)).toEqual(true);
     });
 
-    it('renders getnine button click message', () => {
-        const wrapper = shallow(<App />);
-        wrapper.find('#buttonNine').simulate('click');
-        const nineSign = <p className="App-intro">nine: 9</p>;
-        expect(wrapper.contains(nineSign)).toEqual(true);
+    var doGetTest = (val) => {
+        it('renders get' + val.upperName + " button click message", () => {
+            const wrapper = shallow(<App />);
+            wrapper.find('#button' + val.upperName).simulate('click');
+            const numberElement = <p id={"para" + val.name}>{val.name}:
+                {val.short}</p>;
+            expect(wrapper.contains(numberElement)).toEqual(true);
+        })
+    }
+
+    simNames.forEach((val) => {
+        doGetTest(val);
     });
+
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonOne').simulate('click');
+    //     const numberElement = <p className="App-intro" id="paraone">one: 1</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonTwo').simulate('click');
+    //     const numberElement = <p className="App-intro">two: 2</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonThree').simulate('click');
+    //     const numberElement = <p className="App-intro">three: 3</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonFour').simulate('click');
+    //     const numberElement = <p className="App-intro">four: 4</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonFive').simulate('click');
+    //     const numberElement = <p className="App-intro">five: 5</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonSix').simulate('click');
+    //     const numberElement = <p className="App-intro">six: 6</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonSeven').simulate('click');
+    //     const numberElement = <p className="App-intro">seven: 7</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonEight').simulate('click');
+    //     const numberElement = <p className="App-intro">eight: 8</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
+    //
+    // it('renders getnine button click message', () => {
+    //     const wrapper = shallow(<App />);
+    //     wrapper.find('#buttonNine').simulate('click');
+    //     const numberElement = <p className="App-intro">nine: 9</p>;
+    //     expect(wrapper.contains(numberElement)).toEqual(true);
+    // });
 
     it('renders getFoo button click message', () => {
         const wrapper = shallow(<App />);
