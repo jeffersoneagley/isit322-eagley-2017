8a56b263cbdfff71999a4636252763b61d332005
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-05-13 01:39:50 -0700

    fixed a wrong word in menu, refactored menu out of elfheader

--- Source/GitExplorer/client/src/components/Header/ElfHeader.js
+++ Source/GitExplorer/client/src/components/Header/ElfHeader.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
 import logo from "../../img/Fish_icon_grey.svg";
-import {Link} from "react-router-dom";
 import "../../css/Header/Header.css";
 import "../../css/App.css";
+import ElfHeaderMenu from "./ElfHeaderMenu";
 
 
 class ElfHeader extends Component {
@@ -10,13 +10,7 @@ class ElfHeader extends Component {
     render() {
         return (
             <div className="App-header">
-                <ul className="elf-header-menu">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/get-foo">BarFoo</Link></li>
-                    <li><Link to="/get-numbers">Numbers</Link></li>
-                    <li><Link to="/get-gist">Numbers</Link></li>
-                </ul>
-
+                <ElfHeaderMenu/>
                 <img src={logo} className="App-logo" alt="logo"/>
                 <h2>Welcome to Jefferson's React site</h2>
             </div>
