bdbbc20b00e63ead51b3ac4826ab6f61483ded4d
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-05-13 18:37:30 -0700

    double the minimum tests, plus one, YAY AUTOGENERATION

--- Source/GitExplorer/client/__tests__/GitGood/GetUserInfo.test.js
+++ Source/GitExplorer/client/__tests__/GitGood/GetUserInfo.test.js
@@ -6,64 +6,79 @@ import GetUserInfo from '../../src/components/GitGood/GetUserInfo';
 import fieldDefMocks from '../../__mocks__/mock-field-definitions';
 
 describe('Test suite for GetUserInfo functionality', function() {
-  let fieldDefinitions = [];
-  let gitUser = {};
+      let fieldDefinitions = [];
+      let gitUser = {};
 
-  function gitUserInit() {
-    const tempGitUser = {};
-    for (let value in fieldDefMocks) {
-      let field = fieldDefMocks[value];
-      tempGitUser[field.id] = field.sample;
-    }
-    return tempGitUser;
-  };
+      function gitUserInit() {
+        const tempGitUser = {};
+        for (let value in fieldDefMocks) {
+          let field = fieldDefMocks[value];
+          tempGitUser[field.id] = field.sample;
+        }
+        return tempGitUser;
+      };
 
-  beforeEach(function() {
-    fieldDefinitions = fieldDefMocks;
-    gitUser = gitUserInit();
-  });
+      beforeEach(function() {
+        fieldDefinitions = fieldDefMocks;
+        gitUser = gitUserInit();
+      });
 
-  // let simNames = JSON.parse(mockData('/api/user').body);
+      // let simNames = JSON.parse(mockData('/api/user').body);
 
-  it('renders component without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<GetUserInfo />, div);
-  });
+      it('renders component without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<GetUserInfo />, div);
+      });
 
-  function runTestCycleOnValue(id, value) {
-    it('Renders ' + id + ' paragraph containing ' + value, () => {
-      // console.log(fieldDefinitions);
-      const wrapper = mount(<GetUserInfo
-          fields={fieldDefinitions}
-          gitUser={gitUser}
-          onGetUserButtonClicked={function() {
-            result = true;
-          }}
-      />);
+      function runTestCycleOnValue(id, value) {
+        it('Renders ' + id + ' paragraph containing ' + value.sample, () => {
+          // console.log(fieldDefinitions);
+          const wrapper = mount(<GetUserInfo
+              fields={fieldDefinitions}
+              gitUser={gitUser}
+              onGetUserButtonClicked={function() {
+                result = true;
+              }}
+          />);
 
-      expect(wrapper.find('#' + id).text()).toEqual(value);
-    });
+          expect(wrapper.find('#' + id).text()).toEqual(value.sample);
+        });
 
-    it('simulates button click', () => {
-      let result = false;
-      const wrapper = mount(<GetUserInfo
-          fields={fieldDefinitions}
-          gitUser={gitUser}
-          onGetUserButtonClicked={function() {
-            result = true;
-          }}
-      />);
-      wrapper.find('button').simulate('click');
-      expect(result).toEqual(true);
-    });
-  }
+        it('Renders ' + id + ' label containing ' + value.label, () => {
+          // console.log(fieldDefinitions);
+          const wrapper = mount(<GetUserInfo
+              fields={fieldDefinitions}
+              gitUser={gitUser}
+              onGetUserButtonClicked={function() {
+                result = true;
+              }}
+          />);
+          expect(wrapper.find('[htmlFor=\"' + id + '\"]').text()).
+              toEqual(value.label + ':');
+        });
 
-  function runAllTestsOnGitUserFields() {
-    for (let field in fieldDefMocks) {
-      runTestCycleOnValue(fieldDefMocks[field].id, fieldDefMocks[field].sample);
-    }
-  }
+        it('simulates button click', () => {
+          let result = false;
+          const wrapper = mount(<GetUserInfo
+              fields={fieldDefinitions}
+              gitUser={gitUser}
+              onGetUserButtonClicked={function() {
+                result = true;
+              }}
+          />);
+          wrapper.find('button').simulate('click');
+          expect(result).toEqual(true);
+        });
+      }
 
-  runAllTestsOnGitUserFields();
-  // runTestCycleOnValue('company', 'company-unknown');
-});
+      function runAllTestsOnGitUserFields() {
+        for (let field in fieldDefMocks) {
+          runTestCycleOnValue(fieldDefMocks[field].id, fieldDefMocks[field]);
+        }
+      }
+
+      runAllTestsOnGitUserFields();
+// runTestCycleOnValue('company', 'company-unknown');
+    },
+)
+;
