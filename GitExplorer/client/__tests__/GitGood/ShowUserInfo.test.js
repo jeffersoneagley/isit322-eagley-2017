17ad4b4d64442afc9d7f6491f01901b349e0ece6
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-05-14 20:19:15 -0700

    updates, byebye GetUserInfo

--- GitExplorer/client/__tests__/GitGood/ShowUserInfo.test.js
+++ GitExplorer/client/__tests__/GitGood/ShowUserInfo.test.js
@@ -1,61 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Paragraph from "../src/components/paragraph";
-import ShowUserInfo from '../../src/components/GitGood/ShowUserInfo';
-import fieldDefMocks from '../../__mocks__/mock-field-definitions';
 import {mount} from 'enzyme';
+import fieldDefMocks from '../../__mocks__/mock-field-definitions';
+import ShowUserInfo from '../../src/components/GitGood/ShowUserInfo';
 
-describe('Test suite for ShowUserInfo functionality', function() {
+describe('Test suite for GetUserInfo functionality', function() {
+        let fieldDefinitions = [];
+        let gitUser = {};
 
-    let fieldDefinitions = [];
-    let gitUser = {};
+        function gitUserInit() {
+            const tempGitUser = {};
+            for (let value in fieldDefMocks) {
+                let field = fieldDefMocks[value];
+                tempGitUser[field.id] = field.sample;
+            }
+            return tempGitUser;
+        };
 
-    function gitUserInit() {
-        const tempGitUser = {};
-        for (let value of fieldDefinitions) {
-            if (fieldDefinitions.hasOwnProperty(value)) {
-                tempGitUser[value.id] = value.sample;
+        beforeEach(function() {
+            fieldDefinitions = fieldDefMocks;
+            gitUser = gitUserInit();
+        });
+
+        // let simNames = JSON.parse(mockData('/api/user').body);
+
+        it('renders component without crashing', () => {
+            const div = document.createElement('div');
+            ReactDOM.render(<ShowUserInfo />, div);
+        });
+
+        function runTestCycleOnValue(id, value) {
+            it('Renders ' + id + ' paragraph containing ' + value.sample, () => {
+                // console.log(fieldDefinitions);
+                const wrapper = mount(<ShowUserInfo
+                    fields={fieldDefinitions}
+                    gitUser={gitUser}
+                    onGetUserButtonClicked={function() {
+                        result = true;
+                    }}
+                />);
+
+                expect(wrapper.find('#' + id).text()).toEqual(value.sample);
+            });
+
+            it('Renders ' + id + ' label containing ' + value.label, () => {
+                // console.log(fieldDefinitions);
+                const wrapper = mount(<ShowUserInfo
+                    fields={fieldDefinitions}
+                    gitUser={gitUser}
+                    onGetUserButtonClicked={function() {
+                        result = true;
+                    }}
+                />);
+                expect(wrapper.find('[htmlFor=\"' + id + '\"]').text()).
+                    toEqual(value.label + ':');
+            });
+        }
+
+        it('simulates button click', () => {
+            let result = false;
+            const wrapper = mount(<ShowUserInfo
+                fields={fieldDefinitions}
+                gitUser={gitUser}
+                onGetUserButtonClicked={function() {
+                    result = true;
+                }}
+            />);
+            wrapper.find('button').simulate('click');
+            expect(result).toEqual(true);
+        });
+        function runAllTestsOnGitUserFields() {
+            for (let field in fieldDefMocks) {
+                runTestCycleOnValue(fieldDefMocks[field].id, fieldDefMocks[field]);
             }
         }
-        return tempGitUser;
-    };
 
-    beforeEach(function() {
-        fieldDefinitions = fieldDefMocks;
-        gitUser = gitUserInit();
-    });
-
-    it('renders component without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<ShowUserInfo />, div);
-    });
-
-    // for (let item in data.nameList) {
-    //
-    //     it('Shows dummy user\'s ' + item, () => {
-    //         let result = false;
-    //         const wrapper = mount(<ShowUserInfo
-    //             fields={fieldDefinitions}
-    //             gitUser={gitUser}
-    //             onGetUserButtonClicked={function () {
-    //                 result = true;
-    //             }}
-    //         />);
-    //         expect(wrapper.find(data.gitUser[data.nameList[item]]).count > 0).toEqual(true);
-    //         expect(wrapper.find(data.nameList[item]).count > 0).toEqual(true);
-    //     });
-    // }
-
-    it('simulates button click', () => {
-        let result = false;
-        const wrapper = mount(<ShowUserInfo
-            fields={fieldDefinitions}
-            gitUser={gitUser}
-            onGetUserButtonClicked={function() {
-                result = true;
-            }}
-        />);
-        wrapper.find('button').simulate('click');
-        expect(result).toEqual(true);
-    });
-});
+        runAllTestsOnGitUserFields();
+        // runTestCycleOnValue('company', 'company-unknown');
+    },
+)
+;
