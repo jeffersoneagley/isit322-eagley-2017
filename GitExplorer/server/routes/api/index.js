a694580e730b89c2e24dff868bb60cf863ae6a47
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2017-05-25 18:53:08 -0700

    I got my first microservice working for GitExplorer

--- GitExplorer/server/routes/api/index.js
+++ GitExplorer/server/routes/api/index.js
@@ -3,6 +3,8 @@ let router = express.Router();
 let Git = require('./git');
 let Users = require('./users');
 let morgan = require('morgan')('api-index');
+let requester = require('request');
+let serverConfig = require('../../../config/microserviceAddresses.json');
 
 /* GET home page. */
 router.get('/foo', function(request, response, next) {
@@ -11,9 +13,18 @@ router.get('/foo', function(request, response, next) {
     response.send(message);
 });
 
+getAddress = (serverName) => {
+    return serverConfig[serverName].url ||
+        'http://localhost:' + serverConfig[serverName].port;
+};
+
 router.use('/user', Users);
 
 router.use('/git', Git);
+
+router.get('/qux', (req, res) => {
+    requester(getAddress('qux')).pipe(res);
+});
 
 router.get('/', (request, response, next) => {
     response.status(200).send('<p>this is an API</p>');
